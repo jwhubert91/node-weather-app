01b2467a386e6ca0365e4163b1db97f06ed07748
james w hubert <jwhubert91@gmail.com>
2020-03-11 23:27:18 -0400

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
 		messageTwo.innerHTML = '';
 	} else {
 
-		fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+		fetch(`/weather?address=${location}`).then((response) => {
 			response.json().then((data) => {
 				messageTwo.innerHTML = '';
 				if (data.error) {
